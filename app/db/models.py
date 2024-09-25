b12df768d2cbaa1699b290fd53961203dd4dc389
modeste kouassi <modeste.kouassi@workace.fr>
2024-09-25 11:07:09 +0200

    Import csv

--- app/db/models.py
+++ app/db/models.py
@@ -10,25 +10,59 @@ metadata = Base.metadata
 class User(Base):
     __tablename__ = 'users'
     id = Column(Integer, primary_key=True)
-    name = Column(String(50))
+    lastname = Column(String(50))
+    firstname = Column(String(50))
+    image = Column(String(50))
     email = Column(String(50))
     phone = Column(String(50))
     hashed_password = Column(String(50))
     is_active = Column(Boolean, default=True)
     
     # Relation avec le modèle Rents (emprunts)
-    #rents = relationship("Rents", back_populates="users")
+    rents = relationship("Rent", back_populates="user")
+
+class Author(Base):
+    __tablename__ = 'authors'
+    id = Column(Integer, primary_key=True)
+    name = Column(String(50), nullable=False)
+    image = Column(String(50), nullable=False)
+
+    books = relationship('Book', back_populates='author')
+
+
+class Category(Base):
+    __tablename__ = 'categories'
+    id = Column(Integer, primary_key=True)
+    name = Column(String(50), nullable=False)
+
+    books = relationship('Book', back_populates='category')
+
+
+class BookCategory(Base):
+    __tablename__ = 'books_categories'
+    id = Column(Integer, primary_key=True)
+    category_id = Column(Integer, ForeignKey("categories.id"))
+    book_id = Column(Integer, ForeignKey("books.id"))
+
 
 class Book(Base):
     __tablename__ = 'books'
     id = Column(Integer, primary_key=True)
     title = Column(String(50))
-    author = Column(String(50))
+    author_id = Column(Integer, ForeignKey('authors.id'), nullable=False)
     publication_date = Column(Date)
     description = Column(String(255))
     available = Column(Boolean, default=True)
+    image = Column(String(250))
+    edition = Column(String(250))
+    isbn = Column(Integer(50))
+    publisher = Column(String(250))
 
-class Rents(Base):
+    author = relationship('Author', back_populates='books')
+    rents = relationship('Rent', back_populates='book')
+
+
+class Rent(Base):
     __tablename__ = 'rents'
     id = Column(Integer, primary_key=True)
     user_id = Column(Integer, ForeignKey("users.id"))
@@ -37,5 +71,5 @@ class Rents(Base):
     return_date = Column(Date, nullable=True)
     
     # Relations avec User et Book
-    #user = relationship("User", back_populates="rents")
-    #book = relationship("Book", back_populates="rents")
+    user = relationship('User', back_populates='rents')
+    book = relationship('Book', back_populates='rents')
